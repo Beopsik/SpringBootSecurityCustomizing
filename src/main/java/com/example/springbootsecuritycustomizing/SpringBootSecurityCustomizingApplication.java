5c0103b4c70e1f49adae6044817b2200d14f065c
BeobSik <tjswl756@naver.com>
2021-08-25 14:59:30 +0900

    init

---
+++ src/main/java/com/example/springbootsecuritycustomizing/SpringBootSecurityCustomizingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritycustomizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityCustomizingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityCustomizingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsecuritycustomizing/SpringBootSecurityCustomizingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritycustomizing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityCustomizingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
